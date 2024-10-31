2a20718b23187f156b38e647d71065693f4c5a9c
abhijeetkuma <abhijeetkuma@gmail.com>
2024-10-31 18:17:35 +0530

    college details page updated

--- client/src/Pages/website/college/details.jsx
+++ client/src/Pages/website/college/details.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import PropTypes from "prop-types";
+import { getImageURL } from "../../../utils/utils-image";
 
 import adsImg from "/images/ads.svg";
 import mapIcon from "/images/map-icon.png";
 import star from "/images/star.png";
-
 
 function Collegedetails(props) {
   const [displaycollegdetail, setDisplaycollegdetail] = useState({
@@ -30,77 +30,97 @@ function Collegedetails(props) {
   //console.log(cms_url);
   return (
     <>
-    <section className="detailsBanner">
-      <div className="bgColor">
-        <div className="container detaisHead">
+      <section className="detailsBanner">
+        <div className="bgColor">
+          <div className="container detaisHead">
             <div className="image">
-              {/* <img src="#" alt="college logo" /> */}
+              <img
+                src={getImageURL(displaycollegdetail.logo)}
+                alt="college logo"
+              />
             </div>
             <div className="title">
-              <h1>
-                {displaycollegdetail.college_name}
-              </h1>
+              <h1>{displaycollegdetail.college_name}</h1>
             </div>
-        </div>
-        <div className="container otherInfo">
+          </div>
+          <div className="container otherInfo">
             <ul className="rankInfo">
               <li>
-              <span className="location">
-                <img src={mapIcon} alt="" />
-                <span>{'Agra'}</span>
-              </span>
+                <span className="location">
+                  <img src={mapIcon} alt="" />
+                  <span>
+                    {displaycollegdetail.city_name},{" "}
+                    {displaycollegdetail.state_name}
+                  </span>
+                </span>
               </li>
               <li>
-              <span className="clg-rating">
-                <img src={star} alt="" />
-                <span>4.5 (55)</span>
-              </span>
+                <span className="location">
+                  <span>{displaycollegdetail.college_types}</span>
+                </span>
               </li>
               <li>
-              <span className="nirfRank">
-                <b>#NIRF </b>
-                <span> 5</span>
-              </span>
+                <span className="location">
+                  <span>Est. {displaycollegdetail.found_year}</span>
+                </span>
+              </li>
+              <li>
+                <span className="location">
+                  <span>{displaycollegdetail.approved_by}</span>
+                </span>
+              </li>
+              <li>
+                <span className="clg-rating">
+                  <img src={star} alt="" />
+                  <span>4.5 (55)</span>
+                </span>
+              </li>
+              <li>
+                <span className="nirfRank">
+                  <b>#NIRF </b>
+                  <span> 5</span>
+                </span>
               </li>
             </ul>
+          </div>
         </div>
-      </div>
-
-    </section>
-    <section className="container detailsTab">
-      <ul>
+      </section>
+      <section className="container detailsTab">
+        <ul>
           <li>
-            <Link to='#' className="active">Overview</Link>
+            <Link to="#" className="active">
+              Overview
+            </Link>
           </li>
           <li>
-            <Link to='#'>Courses & Fees</Link>
+            <Link to="#">Courses & Fees</Link>
           </li>
           <li>
-            <Link to='#'>Admission 2024</Link>
+            <Link to="#">Admission 2024</Link>
           </li>
           <li>
-            <Link to='#'>Placements</Link>
+            <Link to="#">Placements</Link>
           </li>
           <li>
-            <Link to='#'>Scholarships</Link>
+            <Link to="#">Scholarships</Link>
           </li>
           <li>
-            <Link to='#'>Faculties</Link>
+            <Link to="#">Faculties</Link>
           </li>
           <li>
-            <Link to='#'>Gallery</Link>
+            <Link to="#">Gallery</Link>
           </li>
           <li>
-            <Link to='#'>Reviews</Link>
+            <Link to="#">Reviews</Link>
           </li>
           <li>
-            <Link to='#'>News</Link>
+            <Link to="#">News</Link>
           </li>
           <li>
-            <Link to='#'>Q&A</Link>
+            <Link to="#">Q&A</Link>
           </li>
-      </ul>
-    </section>
+        </ul>
+      </section>
       <section className="container college-filter-wrapper">
         <section className="college-list-wrapper">
           <div className="font-bold text-2xl pb-3 pt-3">
@@ -111,6 +131,8 @@ function Collegedetails(props) {
               __html: displaycollegdetail.college_descripton,
             }}
           ></div>
+          <div className="font-bold text-2xl pb-3 pt-3">Courses</div>
+          <p>{displaycollegdetail.courses_name}</p>
           <div className="font-bold text-2xl pb-3 pt-3">Faculty Profile</div>
           <div
             dangerouslySetInnerHTML={{
@@ -118,6 +140,12 @@ function Collegedetails(props) {
             }}
           ></div>
           <div className="font-bold text-2xl pb-3 pt-3">Placements</div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: displaycollegdetail.placement_overview,
+            }}
+          ></div>
+
           <ul>
             <li>
               <b>Total Placement Ratio :</b>{" "}
@@ -146,6 +174,25 @@ function Collegedetails(props) {
               <b>Top Profile :</b> {displaycollegdetail.topprofile}
             </li>
           </ul>
+          <div className="font-bold text-2xl pb-3 pt-3">Facility Avaible</div>
+          <p>{displaycollegdetail.facility_available}</p>
+          <div className="font-bold text-2xl pb-3 pt-3">Address</div>
+          <p>
+            {displaycollegdetail.address} {displaycollegdetail.address2}
+            {displaycollegdetail.city_name}, {displaycollegdetail.state_name}{" "}
+            {displaycollegdetail.pincode}
+          </p>
+          <p>
+            {displaycollegdetail.email && "Email: " + displaycollegdetail.email}
+          </p>
+          <p>
+            {displaycollegdetail.contactno &&
+              "Contact: " + displaycollegdetail.contactno}
+          </p>
+          <p>
+            {displaycollegdetail.website &&
+              "Website: " + displaycollegdetail.website}
+          </p>
         </section>
         <div className="others">
           <div className="ads">
