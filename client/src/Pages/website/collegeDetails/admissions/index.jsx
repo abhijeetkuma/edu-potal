485a5b5168ef4c6a0603581fb12c29b32edcc22a
abhijeetkuma <abhijeetkuma@gmail.com>
2024-11-16 18:56:57 +0530

    update

--- client/src/Pages/website/collegeDetails/admissions/index.jsx
+++ client/src/Pages/website/collegeDetails/admissions/index.jsx
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function CollegeAdmissions(props) {
-    return(
-        <>
-            <div className="container detailsTab">
-               <h1>Admissions</h1>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <section className="container college-filter-wrapper">
+        <section className="college-list-wrapper">
+          <div className="container detailsTab">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: props.data.adminssiondetails,
+              }}
+            ></div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 }
 
 export default CollegeAdmissions;
--- client/src/Pages/website/collegeDetails/scholarships/index.jsx
+++ client/src/Pages/website/collegeDetails/scholarships/index.jsx
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function CollegeScholarships(props) {
-    return(
-        <>
-            <div className="container detailsTab">
-               <h1>Scholarships</h1>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <section className="container college-filter-wrapper">
+        <section className="college-list-wrapper">
+          <div className="container detailsTab">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: props.data.scholarshipoffer,
+              }}
+            ></div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 }
 
 export default CollegeScholarships;
