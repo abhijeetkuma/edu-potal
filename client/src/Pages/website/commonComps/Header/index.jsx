8c667a70d458f671b1371d42ca9726d718021764
mridul <mridulkumar555@gmail.com>
2025-01-19 22:25:32 +0530

    responsive menu done

--- client/src/Pages/website/commonComps/Header/index.jsx
+++ client/src/Pages/website/commonComps/Header/index.jsx
@@ -11,6 +11,7 @@ import searchIcon from "/images/search.svg";
 
 function Relatedcolleges(props) {
   const modelStatus = useSelector((state) => state.manageModel.isOpen);
+  const [isMobMenuOpen, setIsMobMenuOpen] = useState(false)
   const dispatch = useDispatch();
 
   // const [isModalOpen, setIsModalOpen] = useState(false);
@@ -26,13 +27,13 @@ function Relatedcolleges(props) {
   return (
     <>
       <header className="website-wrapper">
-        <div className="logo">
+        <div className="desktop logo">
           <a href={`/`}>
             <img src={logo} alt="Edu Potal" />
           </a>
         </div>
         <nav>
-          <ul>
+          <ul className="desktop-menu">
             <li>
               <NavLink
                 to={`/`}
@@ -90,6 +91,78 @@ function Relatedcolleges(props) {
               </span>
             </li>
           </ul>
+          <div className="mobile-menu">
+            <span className={`hamburger-menu ${isMobMenuOpen ? 'open' : 'close'}`} onClick={() => setIsMobMenuOpen(!isMobMenuOpen)}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+            {isMobMenuOpen && 
+              <ul>
+                <li>
+                  <NavLink
+                    to={`/`}
+                    className={`text-sm ${({ isActive }) => (isActive ? "active" : "")}`}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to={`/listing`}
+                    className={`text-sm ${({ isActive }) => (isActive ? "active" : "")}`}
+                  >
+                    Colleges
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to={`/exams`}
+                    className={`text-sm ${({ isActive }) => (isActive ? "active" : "")}`}
+                  >
+                    Exams
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to={`/courses`}
+                    className={`text-sm ${({ isActive }) => (isActive ? "active" : "")}`}
+                  >
+                    Courses
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to={`/studygoal`}
+                    className={`text-sm ${({ isActive }) => (isActive ? "active" : "")}`}
+                  >
+                    Study Goal
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to={`/toccafe`}
+                    className={`text-sm ${({ isActive }) => (isActive ? "active" : "")}`}
+                  >
+                    TOC Cafe
+                  </NavLink>
+                </li>
+              </ul>
+            }
+            <div className="logo">
+              <a href={`/`}>
+                <img src={logo} alt="Edu Potal" />
+              </a>
+            </div>
+            <div>
+              <span
+                className="globalSearch"
+                onClick={() => dispatch(openModel())}
+              >
+                <img src={searchIcon} alt="Global Search" />
+              </span>
+            </div>
+          </div>
         </nav>
         <div className="profile">{/* <Link to="/login">Login</Link> */}</div>
       </header>
