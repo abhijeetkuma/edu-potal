8bb6f2e42c1e6de153f36a43ab40fb04eac67367
abhijeetkuma <abhijeetkuma@gmail.com>
2025-04-27 14:00:02 +0530

    update news

--- client/src/Pages/newsarticles.jsx
+++ client/src/Pages/newsarticles.jsx
@@ -534,6 +534,24 @@ function Newsarticles() {
               htmlFor="na_metakeyword"
               className="block text-sm font-bold leading-6 text-gray-900"
             >
+              Author / Posted By
+            </label>
+            <input
+              type="text"
+              name="na_metakeyword"
+              id="na_metakeyword"
+              value={editdata.na_metakeyword ? editdata.na_metakeyword : ""}
+              required="required"
+              onChange={handleChangeFormdata}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+            <div className="errmsg">{errorMsg[0]}</div>
+          </div>
+          <div className="mt-2">
+            <label
+              htmlFor="na_metakeyword"
+              className="block text-sm font-bold leading-6 text-gray-900"
+            >
               Image
             </label>
             <input
