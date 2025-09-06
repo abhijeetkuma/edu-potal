b44a306acbb4ea575a62a0bde496179637e5429f
abhijeetkuma <abhijeetkuma@gmail.com>
2025-09-06 19:22:36 +0530

    db link update for getmykee

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   port: 3007,
   api_link: "http://localhost",
-  dbhost: "164.121.168.184.host.secureserver.net",
+  //dbhost: "164.121.168.184.host.secureserver.net",
+  dbhost: "tocdb-prd.getmykee.com",
   dbname: "tocprddb",
   dbuser: "tocadmin",
   dbpassword: "Avi@1985",
